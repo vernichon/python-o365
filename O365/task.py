c0ec9500d14794e4a43bdeb795c703facc0243eb
Eric Vernichon <eric@vernichon.fr>
2016-06-28 16:16:35 +0200

    Create Task

--- O365/task.py
+++ O365/task.py
@@ -11,13 +11,21 @@ logging.basicConfig(filename='o365.log',level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 class Task(object):
+    time_string = '%Y-%m-%dT%H:%M:%SZ'
+    #takes a calendar ID
+    create_url = 'https://outlook.office365.com/api/beta/me/tasks'
+    #takes current event ID
+    update_url = 'https://outlook.office365.com/api/v1.0/me/events/{0}'
+    #takes current event ID
+    delete_url = 'https://outlook.office365.com/api/v1.0/me/events/{0}'
+
 
     def __init__(self, json=None, auth=None):
         if json:
             self.json = json
 
         else:
-            self.json = {'Task':{'Body':{}}}
+            self.json = { 'Body':{}}
 
 
         self.auth = auth
@@ -25,8 +33,83 @@ class Task(object):
     def getCreatedTime(self):
         return self.json['CreatedDateTime']
 
+    def getDueDate(self):
+        return self.json['DueDateTime']['DateTime']
+
+
+    def getId(self):
+        return self.json['Id']
+
     def getOwner(self):
         return self.json['Owner']
 
     def getSubject(self):
         return self.json['Subject'].encode('utf-8')
+
+    def getBody(self):
+        return self.json['Body']['Content'].encode('utf-8')
+
+    def setBody(self, val):
+        self.json['Body'] = {'Content' : val,"ContentType":"HTML"}
+
+    def setSubject(self, val):
+        self.json['Subject']=  val
+
+    def setDueDate(self, val, TZ="UTC"):
+        if isinstance(val, time.struct_time):
+            self.json['DueDateTime'] = {"DateTime":time.strftime(self.time_string, val), "TimeZone":TZ}
+        elif isinstance(val, int):
+            self.json['DueDateTime'] = {"DateTime":time.strftime(self.time_string, time.gmtime(val)), "TimeZone":TZ}
+        elif isinstance(val, float):
+            self.json['DueDateTime'] = {"DateTime":time.strftime(self.time_string, time.gmtime(val)), "TimeZone":TZ}
+        else:
+            # this last one assumes you know how to format the time string. if it brakes, check
+            # your time string!
+            self.json['DueDateTime'] = {"DateTime":val, "TimeZone":TZ}
+        print "self.json['DueDateTime']  ",self.json['DueDateTime']
+
+    def getTask(self, task_id):
+        log.debug('fetching task %s .' % task_id)
+        response = requests.get("%s('%s')" % (self.create_url, task_id), auth=self.auth)
+        log.info('Response from O365: %s', str(response))
+
+        return Task(response.json(),self.auth)
+
+
+    def create(self):
+        '''
+        this method creates an event on the calender passed.
+
+        IMPORTANT: It returns that event now created in the calendar, if you wish
+        to make any changes to this event after you make it, use the returned value
+        and not this particular event any further.
+
+        calendar -- a calendar class onto which you want this event to be created. If this is left
+        empty then the event's default calendar, specified at instancing, will be used. If no
+        default is specified, then the event cannot be created.
+
+        '''
+        if not self.auth:
+            log.debug('failed authentication check when creating event.')
+            return False
+
+
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+
+        log.debug('creating json for request.')
+        data = json.dumps(self.json)
+
+        response = None
+        try:
+            log.debug('sending post request now')
+            response = requests.post(self.create_url , data, headers=headers, auth=self.auth)
+            log.debug('sent post request.')
+        except Exception as e:
+            if response:
+                log.debug('response to event creation: %s', str(response))
+            else:
+                log.error('No response, something is very wrong with create: %s', str(e))
+            return False
+
+        log.debug('response to event creation: %s', str(response))
+        return Task(response.json(), self.auth)
